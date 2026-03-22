94f64ef19c8144ae791f52bcab0b56aab8bb81e1
unknown <https://github.com/AlessandrooGomess>
2026-03-22 15:57:15 -0300

    PPag Home finalizada//Implementado estilos Tailwind//espacamentos//tamanhos//ultilizado grid

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,9 +3,11 @@ import { ReactNode } from "react";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid w-full max-w-400 grid-rows[min-content_max-content] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <div className="mx-auto grid min-h-screen w-full max-w-420 grid-rows-[min-content_1fr] gap-5 p-8">
+  <Header />
+  <main className="h-full">
+    {children}
+  </main>
+</div>
   );
 }
